890a505e967afacf57d8ef71f8824870c46ca921
aweyer2025 <aweyer2021@gmail.com>
2023-04-05 21:34:29 -0400

    4-5_Complete

--- #13.py
+++ #13.py
@@ -1,3 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        convert={"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+        roman={"I" : 1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        num_out=0
+        s = s.replace("IV", "IIII").replace("IX","VIIII")\
+            .replace("XL","XXXX").replace("XC","LXXXX")\
+            .replace("CD","CCCC").replace("CM","DCCCC")
+        for str in s:
+            num_out+=roman[str]
+        return num_out
